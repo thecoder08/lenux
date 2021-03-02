cc8a443b68d52b331d8f56751719c1215ebcefa3
Lennon McLean <lmmclean08@gmail.com>
2021-03-02 12:42:39 -0500

    updated build.sh

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ sudo rsync -a node/ /mnt/lenux
 sudo rsync -a cat/ /mnt/lenux
 sudo rsync -a bash/ /mnt/lenux
 sudo rsync -a echo/ /mnt/lenux
+sudo rsync -a write/ /mnt/lenux
 echo "Installing GRUB..."
 sudo grub-install --target=i386-pc --boot-directory=/mnt/lenux/boot /dev/loop8
 echo "Finishing up..."
