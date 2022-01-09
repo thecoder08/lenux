8811d6179ae7ecc26a6879af93e22982c17dcc70
Lennon McLean <lmmclean08@gmail.com>
2022-01-09 08:30:25 -0500

    fixed workflow

--- buildlinux.sh
+++ buildlinux.sh
@@ -9,6 +9,7 @@ rm linux-$KERNEL_VERSION.tar.xz
 cp config linux-$KERNEL_VERSION/.config
 cd linux-$KERNEL_VERSION
 make bzImage -j $CORES
+mkdir ../kernel/boot
 mv arch/x86/boot/bzImage ../kernel/boot/linux
 cd ..
 rm -r linux-$KERNEL_VERSION
