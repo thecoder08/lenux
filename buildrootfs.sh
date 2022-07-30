f234be41ddcf2cc4b89018f67dadf1269661d0bb
Lennon McLean <lmmclean08@gmail.com>
2022-07-30 12:12:18 -0400

    fixed scripts and added logo

--- buildrootfs.sh
+++ buildrootfs.sh
@@ -4,30 +4,30 @@ echo "Welcome to the Lenux 1.0.3 build script!"
 echo "Setting up..."
 mkdir rootfs
 echo "Installing programs..."
-sudo rsync -a dirs/ rootfs
-sudo rsync -a doc/ rootfs
-sudo rsync -a init/ rootfs
-sudo rsync -a gnu/ rootfs
-sudo rsync -a lpm/ rootfs
-sudo rsync -a ls/ rootfs
-sudo rsync -a lsh/ rootfs
-sudo rsync -a node/ rootfs
-sudo rsync -a cat/ rootfs
-sudo rsync -a clear/ rootfs
-sudo rsync -a echo/ rootfs
-sudo rsync -a write/ rootfs
-sudo rsync -a cp/ rootfs
-sudo rsync -a rm/ rootfs
-sudo rsync -a nasm/ rootfs
-sudo rsync -a coyote/ rootfs
-sudo rsync -a grady-game/ rootfs
-sudo rsync -a getty/ rootfs
-sudo rsync -a mv/ rootfs
-sudo rsync -a mkdir/ rootfs
-sudo rsync -a grep/ rootfs
+rsync -a dirs/ rootfs
+rsync -a doc/ rootfs
+rsync -a init/ rootfs
+rsync -a gnu/ rootfs
+rsync -a lpm/ rootfs
+rsync -a ls/ rootfs
+rsync -a lsh/ rootfs
+rsync -a node/ rootfs
+rsync -a cat/ rootfs
+rsync -a clear/ rootfs
+rsync -a echo/ rootfs
+rsync -a write/ rootfs
+rsync -a cp/ rootfs
+rsync -a rm/ rootfs
+rsync -a nasm/ rootfs
+rsync -a coyote/ rootfs
+rsync -a grady-game/ rootfs
+rsync -a getty/ rootfs
+rsync -a mv/ rootfs
+rsync -a mkdir/ rootfs
+rsync -a grep/ rootfs
 echo "Creating archive..."
 cd rootfs
-tar -czvf lenux.tar.gz $(ls)
+tar -czvf ../lenux.tar.gz $(ls)
 echo "Finishing up..."
 cd ..
 rm -r rootfs
--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ echo "Finishing up..."
 sudo umount /mnt/lenux
 sudo rmdir /mnt/lenux
 echo "Running test image..."
-qemu-system-x86_64 -m 2048 -vga std -hda test.img -kernel kernel/boot/linux -append "root=/dev/sda rw quiet vga=ask"
+qemu-system-x86_64 -m 2048 -vga std -hda test.img -kernel kernel/boot/linux -append "root=/dev/sda rw vga=0x311"
 echo "Removing test image..."
 rm test.img
 echo "Done!"
