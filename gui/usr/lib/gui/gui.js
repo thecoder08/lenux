619ba95a3b0aa93c1d5dc9a488e3b9a3016c5790
Lennon McLean <lmmclean08@gmail.com>
2022-10-16 16:22:28 -0400

    update gui

--- gui/usr/lib/gui/gui.js
+++ gui/usr/lib/gui/gui.js
@@ -6,26 +6,28 @@ var fb = new Framebuffer('/dev/fb0', 640, 480, 3);
 var x = 0;
 var y = 0;
 
-var mouse = require('fs').readFileSync(__dirname + '/mouse.raw');
+var fs = require('fs');
+var cursor = fs.readFileSync(__dirname + '/cursor.raw');
+var finger = fs.readFileSync(__dirname + '/finger.raw')
 
 getMouse('/dev/input/mice', function(left, middle, right, rel_x, rel_y) {
-    if (((x + rel_x) >= 0) && ((x + rel_x) < 630)) {
+    fb.rectangle(x, y, 20, 20, colors.black);
+    if (((x + rel_x) >= 0) && ((x + rel_x) < 620)) {
         x += rel_x;
     }
-    if (((y - rel_y) >= 0) && ((y - rel_y) < 470)) {
+    if (((y - rel_y) >= 0) && ((y - rel_y) < 460)) {
         y -= rel_y;
     }
-    fb.clear();
     if (left) {
-        fb.rectangle(x, y, 10, 10, colors.red);
+        fb.buffer(x, y, 20, 20, 3, finger);
     }
     else if (middle) {
-        fb.rectangle(x, y, 10, 10, colors.yellow);
+        fb.rectangle(x, y, 20, 20, colors.blue);
     }
     else if (right) {
-        fb.rectangle(x, y, 10, 10, colors.green);
+        fb.rectangle(x, y, 20, 20, colors.green);
     }
     else {
-        fb.buffer(x, y, 20, 20, 3, mouse);
+        fb.buffer(x, y, 20, 20, 3, cursor);
     }
 });
