2c6569029b40628ba262d412937b350da8f92aa5
Lennon McLean <lmmclean08@gmail.com>
2022-10-29 00:32:02 -0400

    updated to linux 6.0.5

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 set -e
-KERNEL_VERSION=6.0.3
+KERNEL_VERSION=6.0.5
 CORES=$(nproc)
 wget https://cdn.kernel.org/pub/linux/kernel/v${KERNEL_VERSION:0:1}.x/linux-$KERNEL_VERSION.tar.xz
 tar -xvf linux-$KERNEL_VERSION.tar.xz
